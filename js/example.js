8d382779e32400951768257a90980d662347fa32
sseol1112 <sseol1112@naver.com>
2025-04-07 16:13:18 +0900

    movie.html 추가
    json 활용한 영화예매시스템 구현
    - html 기본 마크업 프레임 생성 완료
    - json파일 fetch로 호출하여 값 화면에 동적으로 생성되도록 적용

--- js/example.js
+++ js/example.js
@@ -23,40 +23,48 @@
         bindEvtModal: function () {
             var self = this; 
 
-            self.btnOpen.addEventListener("click", function () {                
-                self.dialog.showModal(); // showModal로 열면 모달형식의 팝업 노출, show()로하면 비모달형식의 팝업 노출				
-            });
+            if(self.btnOpen!= null) {
+                self.btnOpen.addEventListener("click", function () {                
+                    self.dialog.showModal(); // showModal로 열면 모달형식의 팝업 노출, show()로하면 비모달형식의 팝업 노출				
+                });
+            }
 
-            self.btnClose.addEventListener("click", function () {
-                self.dialog.close();
-            });
+            if(self.btnClose!= null) {
+                self.btnClose.addEventListener("click", function () {
+                    self.dialog.close();
+                });
+            }
 
-            self.typeName.addEventListener("keydown", function (evt) {				
-                switch (evt.code) {
-                    case "Enter":						
-                        evt.preventDefault();
-                        self.dialog.querySelector(".default").click();
-                        break;
-                }
-            });
+            if(self.typeName!= null) {
+                self.typeName.addEventListener("keydown", function (evt) {
+                    switch (evt.code) {
+                        case "Enter":
+                            evt.preventDefault();
+                            self.dialog.querySelector(".default").click();
+                            break;
+                    }
+                });
+            }
 
-            self.dialog.addEventListener("close", function () {
-                switch (self.dialog.returnValue) {
-                    case "cancel":
-                        return false;
-                    case "negative":
-                        self.message.innerHTML = "당신이 가능할때 언제든지 저에게 말씀해 주세요.";
-                        break;
-                    case "postive":
-                        self.dialog.returnValue = self.typeName.value;
-                        if (self.dialog.returnValue) {
-                            self.message.innerHTML = `<p class='username'>안녕하세요, ${self.dialog.returnValue}님 :)</p>`;
-                        } else {
-                            self.message.innerHTML = "당신은 이름을 가지고 있지 않나요? 만약 그게 아니라면, 당신의 이름을 저에게 말해주기를 원하지 않나요?";
-                        }
-                        break;
-                }
-            });
+            if(self.dialog!= null) {
+                self.dialog.addEventListener("close", function () {
+                    switch (self.dialog.returnValue) {
+                        case "cancel":
+                            return false;
+                        case "negative":
+                            self.message.innerHTML = "당신이 가능할때 언제든지 저에게 말씀해 주세요.";
+                            break;
+                        case "postive":
+                            self.dialog.returnValue = self.typeName.value;
+                            if (self.dialog.returnValue) {
+                                self.message.innerHTML = `<p class='username'>안녕하세요, ${self.dialog.returnValue}님 :)</p>`;
+                            } else {
+                                self.message.innerHTML = "당신은 이름을 가지고 있지 않나요? 만약 그게 아니라면, 당신의 이름을 저에게 말해주기를 원하지 않나요?";
+                            }
+                            break;
+                    }
+                });
+            }
         },
     };
 
